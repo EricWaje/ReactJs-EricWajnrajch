0ca1ee1a44cc670043f319d867273180c7bc6619
Eric Wajnrajch <ericwaje03@gmail.com>
2021-04-15 19:20:33 -0300

    media queries

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CartWidget from '../CartWidget/CartWidget';
+import { FaBars } from 'react-icons/fa';
 import logo from '../../img/logo.svg';
 import './NavBar.css';
 import ButtonPr from '../Button/ButtonPr';
@@ -7,27 +8,26 @@ import ButtonPr from '../Button/ButtonPr';
 const NavBar = () => {
   return (
     <>
-      <nav className="navBar">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-4 d-flex flex-row justify-content-start align-content-center align-items-center">
-              <img src={logo} className="imgLogo"></img>
-            </div>
-            <div className="col-sm-4 d-flex flex-row justify-content-center align-content-center align-items-center">
-              <ul>
-                <li>Home</li>
-                <li>Categorías</li>
-                <li>Contacto</li>
-              </ul>
-            </div>
-            <div className="col-sm-4 d-flex flex-row justify-content-end align-content-center align-items-center">
-              <div className="boton">
-                <ButtonPr info={'Iniciar sesión'} />
-              </div>
-              <div className="divCarrito">
-                <CartWidget />
-              </div>
-            </div>
+      <nav className="navBar d-flex flex-row justify-content-between align-items-center align-content-center">
+        <div className="col-sm-4 d-flex flex-row justify-content-start align-content-center align-items-center">
+          <img src={logo} className="imgLogo"></img>
+        </div>
+        <div className="col-sm-4 d-flex flex-row justify-content-center align-content-center align-items-center">
+          <ul className="menu">
+            <li>Home</li>
+            <li>Categorías</li>
+            <li>Contacto</li>
+          </ul>
+        </div>
+        <div className="col-sm-4 d-flex flex-row justify-content-end align-content-center align-items-center">
+          <div className="boton">
+            <ButtonPr info={'Log in'} />
+          </div>
+          <div className="">
+            <CartWidget />
+          </div>
+          <div className="container-icon">
+            <FaBars />
           </div>
         </div>
       </nav>
